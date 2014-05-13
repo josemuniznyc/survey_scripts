aaf0e7cf949ebe8df8e826e33e436ff7c2ce44e2
José Muñiz <jose@josemuniz.org>
2014-05-13 16:07:07 -0400

    Fixed csv/dataframe issue
    Sometimes you really need to read the docs; don’t assume x files on a
    screen (leave end row call open).

--- response_rate.py
+++ response_rate.py
@@ -8,14 +8,15 @@ import pandas as pd
 import os
 from datetime import date
 
-CONFIG = ['/Users/jmuniz/Dropbox/SPS/S14/Midpoint Response Rate Report/']
+CONFIG = ['/Users/jmuniz/Dropbox/SPS/S14/Midpoint Response Rate Report/Pages/']
 RESPONSE_DATA = []
 
 def convert(html_file):
+    """Take html, convert into list, push to RESPONSE_DATA"""
     dfs = pd.read_html(html_file)
     data = dfs[1]
-    data = data[[0,6]]
-    data = data[1:10]
+    data = data[[0, 6]]
+    data = data[1:]
     data_aslist = list(data.values.tolist())
     RESPONSE_DATA.extend(data_aslist)
     return
@@ -26,14 +27,17 @@ def load_html():
     return surveys
 
 def create_csv(response_list):
-    headers = ['Course','Responses']
-    data = pd.DataFrame(response_list, headers)
+    """Take list of lists, turn into DF, and write out as CSV"""
+
+    headers = ['Course','Number of responses']
+    data = pd.DataFrame(response_list, columns= headers)
     today = date.today()
     csv_file1 = "/Users/jmuniz/Desktop/" + "ResponseRates-"
     csv_file2 = today.strftime("%y%m%d")
     csv_file3 = ".csv"
     csv_filename = csv_file1 + csv_file2 + csv_file3
     data.to_csv(csv_filename, index = False)
+    return
 
 FILES = load_html()
 
@@ -44,4 +48,5 @@ for i in FILES:
         file_toread = CONFIG[0] + i
         convert(file_toread)
 
+print RESPONSE_DATA
 create_csv(RESPONSE_DATA)
